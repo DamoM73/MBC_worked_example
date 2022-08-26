63327f9b3873c5003c0e4a5ec956103f59ec411c
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-08-26 19:36:47 +1000

    stuff'

--- data_store.py
+++ data_store.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 def create_database():
     # connect to the databse and establish the cursor
@@ -23,9 +24,9 @@ def create_database():
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS Renewables(
-            year_id INTEGER,
-            code_id INTEGER,
-            units INTEGER,
+            year_id INTEGER NOT NULL,
+            code_id INTEGER NOT NULL,
+            units INTEGER NOT NULL,
             output REAL,
             PRIMARY KEY (year_id,code_id),
             FOREIGN KEY (code_id) REFERENCES Postcode(code_id)
@@ -39,17 +40,45 @@ def create_database():
         """
         CREATE TABLE IF NOT EXISTS Suburb(
             suburb_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            code_id INTEGER,
-            name TEXT,
+            code_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
             FOREIGN KEY (code_id) REFERENCES Postcode(code_id)
         )
         """
     )
     conn.commit()
+
+
+def populate_db(data_file):
+    """
+    Opens provided data_file and adds it to the database
+    """
     
+    with open(data_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        
+        for row in csv_reader:
+            # finding the columns of required data
+            if row[0] == "Small Unit Installation Postcode":
+                prev_rating_index = -1
+                
+                for index, col in enumerate(row):
+                    if col == "Small Unit Installation Postcode":
+                        postcode_index = index
+                    elif col.endswith("Quantity"):
+                        prev_units_index = index
+                    elif col.endswith("kW"):
+                        prev_rating_index = index
+                    elif col.endswith("Quantity Total"):
+                        units_index = index
+                    elif col.endswith("kW Total"):
+                        rating_index = index
+            if  
+                    
+                        
     
 # ----- CONSTANTS ----- #
-DATASHEETS = [
+DATA_SHEETS = [
     "data/2012_Postcode data for small-scale installations_SGU-Solar.csv",
     "data/2013_Postcode data for small-scale installations_SGU-Solar.csv",
     "data/2014_Postcode data for small-scale installations_SGU-Solar.csv",
@@ -66,4 +95,7 @@ DATABASE = "renewable_power.db"
 
 # ---- MAIN PROGRAM ----
 
-create_database()
+#create_database()
+
+for data_sheet in DATA_SHEETS:
+    populate_db(data_sheet)
