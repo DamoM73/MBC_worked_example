2cbc2247884ad5d6d0995ab76bd55a6d833422e4
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-08-26 21:59:25 +1000

    attempt 1

--- data_store.py
+++ data_store.py
@@ -2,6 +2,9 @@ import sqlite3
 import csv
 
 def create_database():
+    
+    print("Creating database")
+    
     # connect to the databse and establish the cursor
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
@@ -11,8 +14,7 @@ def create_database():
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS Postcode(
-            code_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            postcode TEXT NOT NULL
+            postcode TEXT PRIMARY KEY
         )        
         """
     )
@@ -23,16 +25,17 @@ def create_database():
     # create the Rewables table
     cur.execute(
         """
-        CREATE TABLE IF NOT EXISTS Renewables(
-            year_id INTEGER NOT NULL,
-            code_id INTEGER NOT NULL,
-            units INTEGER NOT NULL,
+        CREATE TABLE IF NOT EXISTS Installs(
+            year INTEGER,
+            postcode TEXT,
+            units INTEGER,
             output REAL,
-            PRIMARY KEY (year_id,code_id),
-            FOREIGN KEY (code_id) REFERENCES Postcode(code_id)
+            PRIMARY KEY (year,postcode),
+            FOREIGN KEY (postcode) REFERENCES Postcode(postcode)
         )
         """
     )
+    
     conn.commit()
     
     # create Suburb table
@@ -40,62 +43,187 @@ def create_database():
         """
         CREATE TABLE IF NOT EXISTS Suburb(
             suburb_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            code_id INTEGER NOT NULL,
+            postcode TEXT NOT NULL,
             name TEXT NOT NULL,
-            FOREIGN KEY (code_id) REFERENCES Postcode(code_id)
+            FOREIGN KEY (postcode) REFERENCES Postcode(postcode)
         )
         """
     )
     conn.commit()
 
 
-def populate_db(data_file):
+def get_postcodes():
+    """
+    uses cursor to add postcodes from the postcode csv file
+    cur: sqlite3 database cursor object
+    """
+    with open(POSTCODES) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        
+        prev_postcode = ""
+        code_id = 0
+        
+        
+        # read each row of the csv file
+        for row in csv_reader:
+            if row[0] != "postcode":
+                postcode = row[0]
+                suburb = row[1]
+                if postcode != prev_postcode:
+                    add_postcode(postcode)
+                    prev_postcode = postcode
+                add_suburb(postcode, suburb)
+
+
+def get_code_id(postcode):
+    # connect to the databse and establish the cursor
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    
+    cur.execute(
+        """
+        SELECT code_id
+        FROM Postcode
+        WHERE postcode = :postcode
+        """,
+        {
+            "postcode":postcode
+        }
+    )
+
+
+def add_postcode(postcode):
+    # connect to the databse and establish the cursor
+    print("Processing postcodes")
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    
+    cur.execute(
+        """
+        INSERT INTO Postcode (postcode)
+        VALUES (:postcode)
+        """,
+        {
+            "postcode":postcode
+        }
+    )
+    conn.commit()
+            
+
+def add_suburb(code_id, suburb):
+    # connect to the databse and establish the cursor
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    
+    cur.execute(
+        """
+        INSERT INTO Suburb (postcode, name)
+        VALUES (:code_id, :suburb)
+        """,
+        {
+            "code_id":code_id,
+            "suburb":suburb
+        }
+    )
+    conn.commit()    
+
+
+def get_install_data(data_file):
     """
     Opens provided data_file and adds it to the database
     """
     
+    print("Processing:",data_file)
+    
     with open(data_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         
-        for row in csv_reader:
-            # finding the columns of required data
-            if row[0] == "Small Unit Installation Postcode":
-                prev_rating_index = -1
-                
-                for index, col in enumerate(row):
-                    if col == "Small Unit Installation Postcode":
-                        postcode_index = index
-                    elif col.endswith("Quantity"):
-                        prev_units_index = index
-                    elif col.endswith("kW"):
-                        prev_rating_index = index
-                    elif col.endswith("Quantity Total"):
-                        units_index = index
-                    elif col.endswith("kW Total"):
-                        rating_index = index
-            if  
+        # read each row of the csv file
+        for index, row in enumerate(csv_reader):
+            # values needed for database
+            postcode = row[0]
+            yr_1_total_units = 0
+            yr_1_total_rating = 0
+            yr_2_total_units = 0
+            yr_2_total_rating = 0
+            if index == 0:
+                year_1 = int(row[3][4:8])
+                if year_1 == 2021:
+                    year_2 = 0
+                else:
+                    year_2 = int(row[27][4:8])
+            elif index > 1:
+                col_index = 3
+                # get year 1 totals
+                while col_index < 26:
+                    yr_1_total_units += int(row[col_index])
+                    col_index += 1
+                    yr_1_total_rating += clean_rating(row[col_index])
+                    col_index += 1
+                # get year 2 totals
+                if year_2 != 0:
+                    while col_index < 51:
+                        yr_2_total_units += int(row[col_index])
+                        col_index += 1
+                        yr_2_total_rating += clean_rating(row[col_index])
+                        col_index += 1
+            
+                if postcode != "0":
+                    add_install_data(year_1,postcode,yr_1_total_units,yr_1_total_rating)
+                    if year_2 != 0:
+                        add_install_data(year_2,postcode,yr_2_total_units,yr_2_total_rating)
+
+
+def add_install_data(year,postcode,units,output):
+    # connect to the databse and establish the cursor
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    
+    cur.execute(
+        """
+        INSERT INTO Installs (year, postcode, units, output)
+        VALUES (:year,:postcode,:units,:output)
+        """,
+        {
+            "year":year,
+            "postcode":postcode,
+            "units":units,
+            "output":output
+        }
+    )
+    conn.commit()
+
                     
+def clean_rating(rating):
+    return float(rating.replace(",",""))
+
+                
+def populate_database():
+        
+    get_postcodes()
+    
+    for data_sheet in DATA_SHEETS:
+        get_install_data(data_sheet)
+    
+        
                         
     
 # ----- CONSTANTS ----- #
 DATA_SHEETS = [
-    "data/2012_Postcode data for small-scale installations_SGU-Solar.csv",
-    "data/2013_Postcode data for small-scale installations_SGU-Solar.csv",
-    "data/2014_Postcode data for small-scale installations_SGU-Solar.csv",
-    "data/2015_Postcode data for small-scale installations_SGU-Solar.csv",
-    "data/2016_Postcode data for small-scale installations_SGU-Solar.csv",
-    "data/2017_data for small-scale installations_SGU-Solar.csv",
-    "data/2018_Postcode data for small-scale installations_all data.csv",
-    "data/2019_Postcode data for small-scale installations_all data.csv",
-    "data/2020_Postcode data for small-scale installations_all data.csv",
-    "data/2021_2022_Postcode data for small-scale installations_SGU-Solar.csv"
+    "data/Postcode data for small-scale installations - SGU-Solar.csv",
+    "data/Postcode data for small-scale installations 2012 - SGU-Solar.csv",
+    "data/Postcode data for small-scale installations 2014 - SGU-Solar.csv",
+    "data/Postcode data for small-scale installations 2016 - SGU-Solar.csv",
+    "data/Postcode data for small-scale installations 2018 - SGU-Solar.csv",
+    "data/Postcode data for small-scale installations 2020 - SGU - Solar.csv"
 ]
 
 DATABASE = "renewable_power.db"
 
+POSTCODES = "data/au_postcodes.csv"
+
 # ---- MAIN PROGRAM ----
 
-#create_database()
+create_database()
 
-for data_sheet in DATA_SHEETS:
-    populate_db(data_sheet)
+populate_database()
